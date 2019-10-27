8ad3630b587c27eb93a65ea017dc2846eb78fa3d
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-10-27 15:31:37 +0530

    1 file modified

--- Popup.js
+++ Popup.js
@@ -7,7 +7,11 @@ class Popup extends React.Component {
       <div className='popup'>
         <div className='popup_inner'>
           <h1>{this.props.text}</h1>
+          <hr />
+          <div style={{float:"right",display:"inline-block"}}>
         <button onClick={this.props.closePopup}>close me</button>
+         <button onClick={this.props.closePopup}>close me</button>
+        </div>
         </div>
       </div>
     );
